23298fcb73d51b67cae2f817f83c3222ee44f732
Gary Chambers <gary@repro.dev>
2025-08-29 21:45:24 +0100

    Add stories for DevTools

--- apps/storybook-ui/.storybook/main.js
+++ apps/storybook-ui/.storybook/main.js
@@ -1,11 +1,10 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../../../packages/*/src/**/*.stories.@(ts|tsx)"],
-  addons: ["@chromatic-com/storybook", "@storybook/addon-docs"],
+  stories: ["../../../packages/*/src/**/*.stories.@(ts|tsx|mdx)"],
+  addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
 };
 export default config;
-
