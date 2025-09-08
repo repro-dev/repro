e0e9339d6a061eee0fc9294d0f9227679792279a
Gary Chambers <gary@repro.dev>
2025-09-08 18:30:45 +0100

    Create dynamic portal mount points

--- packages/design/src/Portal/PortalRootProvider.tsx
+++ packages/design/src/Portal/PortalRootProvider.tsx
@@ -3,7 +3,9 @@ import React, {
   MutableRefObject,
   PropsWithChildren,
   useContext,
+  useEffect,
   useRef,
+  useState,
 } from 'react'
 
 const MAX_INT32 = 2 ** 32 - 1
@@ -33,7 +35,35 @@ export const PortalRootProvider: React.FC<PropsWithChildren> = ({
   )
 }
 
-export function usePortalRoot() {
+function usePortalRoot() {
   const root = useContext(PortalRootContext)
   return root.current ?? null
+}
+
+export function usePortalMountPoint() {
+  const root = usePortalRoot()
+  const [mountPoint, setMountPoint] = useState<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    let elem: HTMLDivElement | null = null
+
+    if (root) {
+      elem = document.createElement('div')
+      elem.style.position = 'fixed'
+      elem.style.top = '0px'
+      elem.style.left = '0px'
+      elem.style.zIndex = `${MAX_INT32}`
+      root.appendChild(elem)
+      setMountPoint(elem)
+    }
+
+    return () => {
+      if (elem) {
+        setMountPoint(null)
+        elem.remove()
+      }
+    }
+  }, [root, setMountPoint])
+
+  return mountPoint
 }
